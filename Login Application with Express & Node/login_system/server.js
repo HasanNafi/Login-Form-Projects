73a3c97a9c2d5441ad791cf5152b0332ceff0f28
Nafi <work.nafi@gmail.com>
2021-10-31 09:39:11 +0600

    login and logout session added

--- Login Application with Express & Node/login_system/server.js
+++ Login Application with Express & Node/login_system/server.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const path = require('path');
+const bodyparser = require("body-parser");
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
+const router = require('./router');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-//home route
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render('base', { title: "Login System" });
+// load static assets
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+
+app.use(session({
+    secret: uuidv4(), //  '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/route', router);
+
+// home route
+app.get('/', (req, res) => {
+    res.render('base', { titl: "Login System" }); //by sensing did mistake the title word for knowing how to handle it to the header section. Just look to the header file in titile section for knowing the correction format.
 })
 
-app.listen(port, () => { console.log("listening to the server on http://localhost:3000") });
+app.listen(port, () => { console.log("Lostening to the server on http://localhost:3000") });
